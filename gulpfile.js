e56094e12ab152a139529590c95fab0bdf9d65c7
Jakob Lingel <jlingel68@gmail.com>
2025-09-07 16:44:40 +0200

    Make gulp check css as well as hbs

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ function zipper(done) {
 
 const cssWatcher = () => watch("assets/css/**", css);
 const jsWatcher = () => watch("assets/js/**", js);
-const hbsWatcher = () => watch(["*.hbs", "partials/**/*.hbs"], hbs);
+const hbsWatcher = () => watch(["*.hbs", "partials/**/*.hbs"], parallel(hbs, css));
 const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
 const build = series(css, js);
 
